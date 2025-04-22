05e85220dc05af84f8c1156939f0f71d1c0770cb
hassan tawfik <j.hassan.tawfik@gmail.com>
2025-04-22 16:22:22 +0200

    first commit

--- main.js
+++ main.js
@@ -1,27 +1,31 @@
-// alert("welecome to js")
-// document.writeln("Welcome")
-// console.log("Enter your name ");
-// int number1;
-// int number2,
-var number1 = +prompt("enter first number? ");
-var number2 = +prompt("enter second number? ");
 
-sum = number1 + number2;
-document.writeln("sum = " + sum + "<br>");
+// 1- Write a program in JavaScript to check the user enter input or not?
 
-Subtraction = number1 - number2;
-document.writeln("Subtraction = " + Subtraction + "<br>");
+    // var input = prompt("enter  input? ");
+    // if (input === null) {
+    //     document.writeln("enter input pls");
 
-multiplication = number1 * number2;
-document.writeln("multiplication = " + multiplication + "<br>");
+    // }
+    // else {
+    //     document.writeln("renter correct input pls");
 
-divion = number1 / number2;
-document.writeln("division = " + divion + "<br>");
+    // }
 
-modelus = number1 % number2;
-document.writeln("modulus = " + modelus + "<br>");
+   // 2- Write a program in JavaScript to check if a number is within a specified range (e.g., 1 to 100).
 
-document.writeln("power = " + Math.pow(number1, number2) + "<br>");
+//    var number =+prompt("enter  number within a specified range (e.g., 1 to 100).");
+//    if (number >=1 && number<=100) {
+//        document.writeln("number in range ");
+//    }
+//    else {
+//        document.writeln("number is not in the range");
+//    }
+   //3.Write a program in JavaScript to check if a person is eligible to vote (age >= 18).
 
-document.writeln(number1 + " is not equal " + number2 + ": " + (number1 != number2) + "<br>");
-document.writeln(number1 + " is greater than " + number2 + ": " + (number1 > number2) + "<br>");
+   var age =+prompt("Enter  age of the person is eligible to vote.age >= 18");
+   if (age >=18 ) {
+       document.writeln("person is eligible to vote ");
+   }
+   else {
+       document.writeln("person is not eligible to vote");
+   }
